4db142a99ef8cfb875e77c0c92b3cdc5fc645b47
Gift <geezyenyoghasim@gmail.com>
2022-05-12 22:42:17 +0100

    added the images

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./index.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="nav-inner">
+        <div className="nav-left">
+          <ul>
+            <li className="nav-item">
+              <a href="#">SO FUNKTIONIERT'S</a>
+            </li>
+            <li className="nav-item">
+              {" "}
+              <a href="#">SONDERANGEBOTE</a>
+            </li>
+            <li className="nav-item">
+              <button>MEIN BEREICH</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
